2847c057b8be23ebb771b080943b494e73a79443
Gustavo Henrique <105286224+GustAmaral@users.noreply.github.com>
2025-04-29 23:40:23 -0300

    Deletando Web/test-bot-project directory
    Apagando projeto bot Telegram

--- Web/test-bot-project/telegram_bot/src/routes/route.jsx
+++
@@ -1,9 +0,0 @@
-import Home from '../pages/Home';
-import About from '../pages/About';
-
-const routes = [
-  { path: '/', name: 'Home', element: <Home /> },
-  { path: '/about', name: 'Sobre', element: <About /> },
-];
-
-export default routes;
